38951b186dbbc65c543bf519ee686e1505c3f7b2
vvasuki <vvasuki@users.noreply.github.com>
2020-12-10 03:33:25 +0000

    deploy: e1293c053aff880e5be4614ee189d802b6b29c64

---
+++ themes/sanskrit-documentation-theme-hugo/webpack_src/js/utils.js
@@ -0,0 +1,25 @@
+
+export function textNodesUnder(node){
+    var all = [];
+    for (node=node.firstChild;node;node=node.nextSibling){
+        if (node.nodeType==3) all.push(node);
+        else all = all.concat(textNodesUnder(node));
+    }
+    return all;
+}
+
+export function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return undefined;
+}
